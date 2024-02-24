0a7bc01c5ae48ddc3d69182c9be1643cacf6f778
Juanes6767 <159493494+Juanes6767@users.noreply.github.com>
2024-02-24 16:10:45 -0500

    Add files via upload

---
+++ Control.java
@@ -0,0 +1,50 @@
+package taller3.televisores;
+
+public class Control {
+	private TV tv;
+
+	public void enlazar(TV tv) {
+		this.tv=tv;
+		tv.setControl(this);
+	}
+
+	public void turnOff() {
+		tv.turnOff();
+	}
+	public void turnOn() {
+		tv.turnOn();
+	}
+	public void setTv(TV tv) {
+		this.tv=tv;
+	}
+	public TV getTv() {
+		return tv;
+	}
+	public void canalUp() {
+		if(tv.getEstado()==true) {
+		tv.canalUp();
+		}
+	}
+	public void canalDown() {
+		if(tv.getEstado()==true) {
+		tv.canalDown();
+		}
+	}
+	public void volumenUp() {
+		if((tv.getEstado()==true) && (tv.getVolumen()<7)) {
+		tv.volumenUp();
+		}
+	}
+	public void volumenDown() {
+		if((tv.getEstado()==true) && (tv.getVolumen()>1)) {
+		tv.volumenDown();
+		}
+	}
+
+	public void setCanal(int canal) {
+		tv.setCanal(canal);
+	}
+	public void setVolumen(int volume) {
+		tv.setVolumen(volume);;
+	}
+}
